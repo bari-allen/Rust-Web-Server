dbd223643173744cf33d70db4fd963c501ba4a93
Karl Haidinyak <karl@hcbed.com>
2024-12-29 15:50:20 -0800

    Changed the tests.rs file and updated the .gitignore file

--- src/tests.rs
+++ src/tests.rs
@@ -1,14 +1,17 @@
-use crate::file_reader::*;
-use std::io::{Error, ErrorKind};
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::file_reader::*;
+    use std::io::{Error, ErrorKind};
+    use std::path::Path;
+
     #[test]
     fn create_file_tests() {
         let result: Result<bool, Error> = create_file();
+        let path = Path::new("./src/users.csv");
+
         if let Err(err) = result {
             assert_eq!(err.kind(), ErrorKind::AlreadyExists);
+            assert!(path.exists());
         } else {
             panic!("Expected an error, but got Ok!");
         }
